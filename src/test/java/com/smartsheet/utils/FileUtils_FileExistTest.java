65a1a6120f16ec4f8c613518d4e2dce34b1e0f34
isim <ihcsim@gmail.com>
2013-12-23 23:24:36 -0800

    Change indentation to 4 spaces.

--- src/test/java/com/smartsheet/utils/FileUtils_FileExistTest.java
+++ src/test/java/com/smartsheet/utils/FileUtils_FileExistTest.java
@@ -18,89 +18,89 @@ import org.junit.Test;
  */
 public class FileUtils_FileExistTest {
 
-  private final static String testFilePrefix = "smartsheet-test-";
-  private final static String testFileSuffix = ".tmp";
-  
-  private File systemTempFolder;
-  private List<File> testFiles;
-  
-  @Before
-  public void setUp(){
-    systemTempFolder = new File(System.getProperty("java.io.tmpdir"));
-    if(!systemTempFolder.exists())
-      systemTempFolder.mkdir();
+    private final static String TEST_FILE_PREFIX = "smartsheet-test-";
+    private final static String TEST_FILE_SUFFIX = ".tmp";
     
-    testFiles = new ArrayList<File>();
-  }
+    private File systemTempFolder;
+    private List<File> testFiles;
   
-  @After
-  public void tearDown(){
-    if(testFiles != null)
-      for(File file : testFiles)
-        file.deleteOnExit();
-  }
-  
-  @Test
-  public void testFileExistsInFolder_EmptyFileName() {
-    Assert.assertTrue(!FileUtils.fileNameExistsInFolder("", systemTempFolder));
-  }
-  
-  @Test(expected=NullPointerException.class)
-  public void testFileExistsInFolder_NullFileName() {
-    FileUtils.fileNameExistsInFolder(null, systemTempFolder);
-    Assert.fail("Should have failed with a NullPointerException.");
-  }
-  
-  @Test
-  public void testFileExistsInFolder_AddOneFileToTempFolder() {
-    try {
-      createUniqueFileInSystemTempFolder();
-      Assert.assertTrue(FileUtils.fileNameExistsInFolder(testFiles.get(0).getName(), systemTempFolder));
-    } catch(IOException e){
-      e.printStackTrace();
-      Assert.fail(e.getMessage());
+    @Before
+    public void setUp(){
+        systemTempFolder = new File(System.getProperty("java.io.tmpdir"));
+        if(!systemTempFolder.exists())
+          systemTempFolder.mkdir();
+        
+        testFiles = new ArrayList<File>();
     }
-  }
   
-  @Test
-  public void testFileExistsInFolder_AddFourFilesToTempFolder() {
-    try {
-      createUniqueFileInSystemTempFolder();
-      createUniqueFileInSystemTempFolder();
-      createUniqueFileInSystemTempFolder();
-      createUniqueFileInSystemTempFolder();
-      Assert.assertTrue(FileUtils.fileNameExistsInFolder(testFiles.get(0).getName(), systemTempFolder));
-    } catch(IOException e){
-      e.printStackTrace();
-      Assert.fail(e.getMessage());
+    @After
+    public void tearDown(){
+        if(testFiles != null)
+            for(File file : testFiles)
+                file.delete();
     }
-  }
   
-  @Test
-  public void testFileExistsInFolder_NewFolderToTempFolder() {
-    try {
-      createNewFolderInSystemTempFolder();
-      Assert.assertTrue(FileUtils.fileNameExistsInFolder(testFiles.get(0).getName(), systemTempFolder));
-    } catch(IOException e){
-      e.printStackTrace();
-      Assert.fail(e.getMessage());
+    @Test
+    public void testFileExistsInFolder_EmptyFileName() {
+        Assert.assertTrue(!FileUtils.fileNameExistsInFolder("", systemTempFolder));
     }
-  }
+    
+    @Test(expected=NullPointerException.class)
+    public void testFileExistsInFolder_NullFileName() {
+        FileUtils.fileNameExistsInFolder(null, systemTempFolder);
+        Assert.fail("Should have failed with a NullPointerException.");
+    }
+  
+    @Test
+    public void testFileExistsInFolder_AddOneFileToTempFolder() {
+        try {
+            createUniqueFileInSystemTempFolder();
+            Assert.assertTrue(FileUtils.fileNameExistsInFolder(testFiles.get(0).getName(), systemTempFolder));
+        } catch(IOException e){
+            e.printStackTrace();
+            Assert.fail(e.getMessage());
+        }
+    }
+  
+    @Test
+    public void testFileExistsInFolder_AddFourFilesToTempFolder() {
+        try {
+            createUniqueFileInSystemTempFolder();
+            createUniqueFileInSystemTempFolder();
+            createUniqueFileInSystemTempFolder();
+            createUniqueFileInSystemTempFolder();
+            Assert.assertTrue(FileUtils.fileNameExistsInFolder(testFiles.get(0).getName(), systemTempFolder));
+        } catch(IOException e){
+            e.printStackTrace();
+            Assert.fail(e.getMessage());
+        }
+    }
+  
+    @Test
+    public void testFileExistsInFolder_NewFolderToTempFolder() {
+        try {
+            createNewFolderInSystemTempFolder();
+            Assert.assertTrue(FileUtils.fileNameExistsInFolder(testFiles.get(0).getName(), systemTempFolder));
+        } catch(IOException e){
+            e.printStackTrace();
+            Assert.fail(e.getMessage());
+        }
+    }
 
-  @Test
-  public void testFileExistsInFolder_NoSuchFileInTempFolder() {
-    File noSuchFile = new File(Long.toString(new Date().getTime()));
-    Assert.assertTrue(!FileUtils.fileNameExistsInFolder(noSuchFile.getName(), systemTempFolder));
-  }
-  
-  private void createUniqueFileInSystemTempFolder() throws IOException{
-    File tempFile = File.createTempFile(testFilePrefix, testFileSuffix);
-    testFiles.add(tempFile);
-  }
-  
-  private void createNewFolderInSystemTempFolder() throws IOException{
-    File tempFile = File.createTempFile(testFilePrefix, testFileSuffix);
-    tempFile.mkdir();
-    testFiles.add(tempFile);
-  }
+    @Test
+    public void testFileExistsInFolder_NoSuchFileInTempFolder() {
+        File noSuchFile = new File(Long.toString(new Date().getTime()));
+        Assert.assertTrue(!FileUtils.fileNameExistsInFolder(noSuchFile.getName(), systemTempFolder));
+    }
+    
+    private void createUniqueFileInSystemTempFolder() throws IOException{
+        File tempFile = File.createTempFile(TEST_FILE_PREFIX, TEST_FILE_SUFFIX);
+        testFiles.add(tempFile);
+    }
+    
+    private void createNewFolderInSystemTempFolder() throws IOException{
+        File tempFile = File.createTempFile(TEST_FILE_PREFIX, TEST_FILE_SUFFIX);
+        tempFile.mkdir();
+        testFiles.add(tempFile);
+    }
 }
